ed5b369fb7170979ccabf1346ce23fa0f66ddb1f
marco-cristofano <marco_cristofano@hotmail.com>
2016-10-21 17:54:58 -0300

    Add files via upload

--- DTH22.c
+++ DTH22.c
@@ -19,122 +19,99 @@
 
 //definir con cuando este el clock
 #define MAXVALUE            4294967296 
-#define TIME_0_MIN          3060  //15Us 
-#define TIME_0_MAX          4140  //35Us
-#define TIME_1_MIN          12240 //60Us
-#define TIME_1_MAX          15096 //74Us
+#define TIME_0_MIN          4488  //22Us 
+#define TIME_0_MAX          6528  //32Us
+#define TIME_1_MIN          13872 //68Us
+#define TIME_1_MAX          15300 //75Us
 
-
- uint32_t subtract=0;
- volatile  uint8_t location=0;
- volatile  uint8_t ubication=0; //borrar
- volatile static uint32_t last_edge;
- volatile static uint32_t current_edge;
-
- //volatile uint8_t information[5]
- volatile uint8_t HR_integer_byte;
- volatile uint8_t HR_decimal_byte; 
- volatile uint8_t T_integer_byte;
- volatile uint8_t T_decimal_byte;
- volatile uint8_t checksum;
-
+//varibles para el funcionamiento logico
+volatile static volatile uint8_t  checksum;;
+volatile static uint32_t subtract=0;
+volatile static uint8_t location=0;
+volatile static uint32_t last_edge;
+volatile static uint32_t current_edge;
+ 
 
 /* Para llamar a esta funcion debemos tener el siguiete codigo, recordar modificar el vector de interrupciones
 __attribute__ ((section(".after_vectors")))
 void TIMER0_IRQHandler(void){ 
     save_bit();
-    Chip_TIMER_ClearCapture(LPC_TIMER, CAP_NUMB);
 }
 */
 
-void DHT22_stop(void){
-	Chip_Clock_Disable(CLK_MX_TIMER0);
-    NVIC_DisableIRQ(LPC_TIMER_IRQ);
-}
-
-
 void DHT22_handler(void){
-	current_edge=Chip_TIMER_ReadCapture(LPC_TIMER, CAP_NUMB);
-    if(current_edge > last_edge)//si nose desborda el contador
+  //---Variables-------------------------------------------
+  uint8_t sum;
+  FINISH=0;
+  //-------------------------------------------------------
+   current_edge=Chip_TIMER_ReadCapture(LPC_TIMER, CAP_NUMB);//recojo el valor de la captura
+   if(current_edge > last_edge)//si nose desborda el contador
         subtract=current_edge - last_edge;
-    else//si se desborda el contador
+   else//si se desborda el contador
         subtract=(MAXVALUE - last_edge + current_edge);
-    last_edge=current_edge;
-    
-    ubication++; //BORRAR
-
-    if (subtract <= TIME_0_MAX && subtract >= TIME_0_MIN){ //si entra es por que es un 0
+   last_edge=current_edge;//se actualiza el EDGE anterior
+   
+   if (subtract <= TIME_0_MAX && subtract >= TIME_0_MIN){ //si entra es por que es un 0
       //Guardo un cero donde corresponda
-         if (location >= 0 && location <= 7)//el bit pertenece a la parte entera de HR
-           HR_integer_byte=(HR_integer_byte<<1);
-         else if (location>=8 && location<=15) //El bit pertenece a la parte decimal de la HR
-           HR_decimal_byte=(HR_decimal_byte<<1);
-         else if ((location >= 16) && (location <= 23))//el bit pertenece a la parte entera de T
-           T_integer_byte=(T_integer_byte<<1);
-         else if (location>=24 && location <=31) //el bit pertenece a la parte decimal de T
-           T_decimal_byte=(T_decimal_byte<<1);
+        if (location >= 0 && location <= 15)//el bit pertenece a la HUMEDAD RELATIVA
+          HUMIDITY_RELATIVE=(HUMIDITY_RELATIVE<<1);
+         else if (location>=16 && location<=31) //el bit pertenece a la TEMPERATURA
+           TEMPERATURE=(TEMPERATURE<<1);
          else //el bit pertenece al Checksum
            checksum=(checksum<<1); 
         location++;    
     }
        else if (subtract <= TIME_1_MAX && subtract >= TIME_1_MIN){ //si entra es por que es un 1
-         //Guardo un 1 donde corresonda
-         if (location >= 0 && location <= 7)//el bit pertence a la parte entera de HR
-           HR_integer_byte=(HR_integer_byte<<1) | 1;
-         else if (location>=8 && location<=15)//El bit pertenece a la parte decimal de la HR
-           HR_decimal_byte=(HR_decimal_byte<<1) | 1;
-         else if (location >= 16 && location <= 23)//el bit pertence a la parte entera de TEMP
-           T_integer_byte=(T_integer_byte<<1) | 1;
-         else if (location>=24 && location <=31) //el bit pertence a la parte decimal de TEMP
-           T_decimal_byte=(T_decimal_byte<<1) | 1;
+        //Guardo un 1 donde corresonda
+         if (location >= 0 && location <= 15)///el bit pertenece a la HUMEDAD RELATIVA
+           HUMIDITY_RELATIVE=(HUMIDITY_RELATIVE<<1) | 1;
+         else if (location>=16 && location<=31)//el bit pertenece a la TEMPERATURA
+           TEMPERATURE=(TEMPERATURE<<1) | 1;
          else //el bit pertence al Checksum
            checksum=(checksum<<1) | 1;   
           location++;   
     }
   if (location==40){//se terminó el frame que envia el DHT22
+    sum=(HUMIDITY_RELATIVE>>8)+HUMIDITY_RELATIVE+(TEMPERATURE>>8)+TEMPERATURE; //suma necesaria para calcular el checksum
+    if(checksum != sum){
+      HUMIDITY_RELATIVE=65535;     
+      TEMPERATURE=65535;
+    }
+    FINISH=1;//indicacion de finalizacion del sensado
     location=0;
     last_edge=0;
     Chip_TIMER_Disable(LPC_TIMER0);  
   }
+ Chip_TIMER_ClearCapture(LPC_TIMER, CAP_NUMB);//limpia la captura ya procesada
 
 }
-void DTH22_init(void){
-	Chip_Clock_Enable(CLK_MX_TIMER0);
-    Chip_SCU_PinMux ( IC_SCU_PORT, IC_SCU_PIN,MD_EZI | MD_PUP, FUNC4);
-    /* Timer0 init */
-    Chip_TIMER_Init(LPC_TIMER);//inicializa el timer      
-    /* Timer 0 CAP0 EDGE enabled and SELECT as 0x2 */
-    LPC_GIMA->CAP0_IN[TIMER_NUMBER][CAP_NUMB] = ((1<<4));
-    /* Reset timer0 */
-    Chip_RGU_TriggerReset(RGU_TIMER_RST);//llama al sistema de reset(RGU) para que resetee el timer0
-    
-    while (Chip_RGU_InReset(RGU_TIMER_RST)); //esta funcion devuelve true si todavia se está reseteando, por lo tanto sale del while cuando se termino el reset
-
-    Chip_TIMER_Reset(LPC_TIMER); //resetea el timer desde el propio timer
-    
-    Chip_TIMER_TIMER_SetCountClockSrc(LPC_TIMER, TIMER_CAPSRC_RISING_PCLK, CAP_NUMB); 
-    /* Select prescaler value */
-    Chip_TIMER_PrescaleSet(LPC_TIMER, TIMER0_PRESCALER);//setea el prescalador
-    /* Clear capture value on T0.CAP0 */
-    Chip_TIMER_ClearCapture(LPC_TIMER, CAP_NUMB);//elimina captura antigua
-    /* Enable rising trigger on T0.CAP0 */
-    Chip_TIMER_CaptureRisingEdgeEnable(LPC_TIMER, CAP_NUMB);//activa la captura de flancos ascendentes
-    Chip_TIMER_CaptureFallingEdgeEnable (LPC_TIMER, CAP_NUMB );//Activo la captura de flanco de bajada
-    Chip_TIMER_CaptureEnableInt(LPC_TIMER, CAP_NUMB);//cada vez que el CAP0 sea cargado el timer lanzara una interrpcion
-    /* Enable timer interrupt */
-    NVIC_ClearPendingIRQ(LPC_TIMER_IRQ);//limpia las interrupciones del timer que pudieron haber quedado por un previo uso(supongo que no hace falta)
-    NVIC_EnableIRQ(LPC_TIMER_IRQ);//activa las interrupciones del timer
-    /* Enable timer */
-    Chip_TIMER_Enable(LPC_TIMER);
-
+void DTH22_sensing(void){
+//Envio del pulso de start----------------------------------------------- 
+Chip_SCU_PinMux ( IC_SCU_PORT, IC_SCU_PIN, MD_EZI | MD_PUP, FUNC0);//seleccion del puerto 66 como GPIO
+Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, OUTPUT);//seleccion del puerto 66(cpio0[12] como salida)
+Chip_GPIO_SetPinState( LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, LOW);//pulso en bajo
+delay_ms(17);//REVISAR VALOr EN EL MANUAL(ES CONFUSO)
+Chip_GPIO_SetPinState( LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, HIGH);//pulso en alto(innecesario teniendo en cuenta la presencia del PULL-UP)
+//Fin del Envio del pulso inicial----------------------------------------
+Chip_Clock_Enable(CLK_MX_TIMER0);//habilita el clock para el TIMER0
+Chip_SCU_PinMux ( IC_SCU_PORT, IC_SCU_PIN,MD_EZI | MD_PUP, FUNC4);//Pin 66 en modo captura
+Chip_TIMER_Init(LPC_TIMER);//inicializa el timer      
+LPC_GIMA->CAP0_IN[TIMER_NUMBER][CAP_NUMB] = ((1<<4));//
+Chip_RGU_TriggerReset(RGU_TIMER_RST);//llama al sistema de reset(RGU) para que resetee el timer0
+while (Chip_RGU_InReset(RGU_TIMER_RST)); //esta funcion devuelve true si todavia se está reseteando, por lo tanto sale del while cuando se termino el reset
+Chip_TIMER_Reset(LPC_TIMER); //resetea el timer desde el propio timer
+Chip_TIMER_TIMER_SetCountClockSrc(LPC_TIMER, TIMER_CAPSRC_RISING_PCLK, CAP_NUMB); 
+Chip_TIMER_PrescaleSet(LPC_TIMER, TIMER0_PRESCALER);//setea el prescalador
+Chip_TIMER_ClearCapture(LPC_TIMER, CAP_NUMB);//elimina captura antigua
+Chip_TIMER_CaptureRisingEdgeEnable(LPC_TIMER, CAP_NUMB);//activa la captura de flancos ascendentes
+Chip_TIMER_CaptureFallingEdgeEnable (LPC_TIMER, CAP_NUMB );//Activo la captura de flanco de bajada
+Chip_TIMER_CaptureEnableInt(LPC_TIMER, CAP_NUMB);//cada vez que el CAP0 sea cargado el timer lanzara una interrpcion
+NVIC_ClearPendingIRQ(LPC_TIMER_IRQ);//limpia las interrupciones del timer que pudieron haber quedado por un previo uso(supongo que no hace falta)
+NVIC_EnableIRQ(LPC_TIMER_IRQ);//activa las interrupciones del timer
+Chip_TIMER_Enable(LPC_TIMER);//activa el timer
 }
-void DTH22_startPulse(void){
 
-	Chip_SCU_PinMux ( IC_SCU_PORT, IC_SCU_PIN, MD_EZI, FUNC0);
-	Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, OUTPUT);
-	Chip_GPIO_SetPinState( LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, LOW);
-	delay_ms(18);//REVISAR VALRO EN EL MANUAL(ES CONFUSO)
-	Chip_GPIO_SetPinState( LPC_GPIO_PORT, GPIO_PORT_OUT, GPIO_PIN_OUT, HIGH);
-
-
+void DHT22_stop(void){ 
+  Chip_Clock_Disable(CLK_MX_TIMER0);
+  NVIC_DisableIRQ(LPC_TIMER_IRQ);
 }
--- DTH22.h
+++ DTH22.h
@@ -3,10 +3,27 @@
 
 #include "stdint.h"
 
+//almacenamiento de las Humedad y Temperatura
+volatile uint16_t HUMIDITY_RELATIVE;
+volatile uint16_t TEMPERATURE;
+volatile uint8_t FINISH; //si FINISH es igual a 1, finalizó el sensado 
 
 void DHT22_handler(void);
-void DTH22_init(void);
+void DTH22_sensing(void);
 void DHT22_stop(void);
-void DTH22_startPulse(void);
+
 
 #endif
+
+/*void DHT22_handler(void); Funcion que se encarga del tratamiento de la interrupcion del imput capture del TIMER0 para el DTH22. Se debe hacer su llamado desde la interrupcion. Para indicar la finalizacion de la interrpcion la varible GLOBAL FINISH se coloca en 1, a partir de ese instante en las variables globales TEMPERATURE y HUMIDITY_RELATIVE se encuentra almacenados los resultados. Si TEMPERATURE Y HUMIDITY_RELATIVE equivales a 65535 significa que el sendado fue erroneo(checksum failed)*/
+/*void DTH22_sensing(void);  Funcion que envia el pulso de start hacia el DTH22 y luego configura todo lo necesario para el INPUTCAPTURE. Se debe llamar por cada ves que se requiera realziar el sensado*/
+/*void DHT22_stop(void); Funcion que se llama en caso de que se desee deshabilitar completamente la captura de flancos del sistema*/
+
+/*CONEXIONADO
+EL voltage de alimentacion típico es de 5V(apto de 3.3V hasta 6V)
+La comunicacion se realiza por medio de un solo puerto, PUERTO 66 o MDIO.
+VER IMAGEN "conexionDHT221" y "conexionDHT22" para mas información
+Se puede agregar un acapacitor de 100uF entre GND y VCC para obtener mayor estabilidad en el sensado
+ACLARACION: en la imagen "conexionDHT222" el calble rojo es 5V, el negro es tierra y el blanco es DATOS
+
+ */
